6edd3cac14fa72305740fc3de93d4e308c8a4830
Sergii Pechenizkyi <plastivreg@gmail.com>
2024-11-27 20:19:46 +0100

    2023 day 1 part 2

--- src/main/kotlin/aoc2023/Day1.kt
+++ src/main/kotlin/aoc2023/Day1.kt
@@ -1,9 +1,63 @@
 package aoc2023
 
-fun sumAllValues(input: List<String>): Number =
+fun part1Solution(input: List<String>): Number =
     input
         .sumOf { line ->
             val firstDigitChar = line.first { it.isDigit() }
             val lastDigitChar = line.last { it.isDigit() }
             "$firstDigitChar$lastDigitChar".toInt()
         }
+
+val validDigits =
+    listOf<String>(
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    )
+
+fun part2Solution(input: List<String>): Number =
+    input
+        .sumOf { line ->
+            val firstDigitChar =
+                mapToDigit(line.findAnyOf(validDigits)!!.second)
+            val lastDigitChar = mapToDigit(line.findLastAnyOf(validDigits)!!.second)
+            "$firstDigitChar$lastDigitChar".toInt()
+        }
+
+private fun mapToDigit(find: String): String =
+    when (find) {
+        "1" -> "1"
+        "2" -> "2"
+        "3" -> "3"
+        "4" -> "4"
+        "5" -> "5"
+        "6" -> "6"
+        "7" -> "7"
+        "8" -> "8"
+        "9" -> "9"
+        "one" -> "1"
+        "two" -> "2"
+        "three" -> "3"
+        "four" -> "4"
+        "five" -> "5"
+        "six" -> "6"
+        "seven" -> "7"
+        "eight" -> "8"
+        "nine" -> "9"
+        else -> error("Shouldn't happen, but was $find")
+    }
--- src/test/kotlin/aoc2023/Day1Test.kt
+++ src/test/kotlin/aoc2023/Day1Test.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 
 class Day1Test {
     @Test
-    fun sumExampleValues() {
+    fun part1ExampleValues() {
         val calibrationInput =
             """
             1abc2
@@ -15,14 +15,38 @@ class Day1Test {
             treb7uchet
             """.trimIndent()
                 .lines()
-        val result = sumAllValues(calibrationInput)
+        val result = part1Solution(calibrationInput)
         assertThat(result).isEqualTo(142)
     }
 
     @Test
-    fun sumInputValues() {
+    fun part1InputValues() {
         val input = fileInput("Day1Input.txt").readLines()
-        val result = sumAllValues(input)
+        val result = part1Solution(input)
         assertThat(result).isEqualTo(54159)
+    }
+
+    @Test
+    fun part2ExampleValues() {
+        val calibrationInput =
+            """
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen    
+            """.trimIndent()
+                .lines()
+        val result = part2Solution(calibrationInput)
+        assertThat(result).isEqualTo(281)
+    }
+
+    @Test
+    fun part2InputValues() {
+        val input = fileInput("Day1Input.txt").readLines()
+        val result = part2Solution(input)
+        assertThat(result).isEqualTo(53866)
     }
 }
