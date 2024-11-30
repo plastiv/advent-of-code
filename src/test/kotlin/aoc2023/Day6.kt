b696434354a23f28e1b9d3a05375978f601d9dde
Sergii Pechenizkyi <plastivreg@gmail.com>
2024-11-30 14:06:28 +0100

    2023 day 6 part 2

--- src/test/kotlin/aoc2023/Day6.kt
+++ src/test/kotlin/aoc2023/Day6.kt
@@ -46,4 +46,60 @@ class Day6 {
                 }.count { travelDistance -> travelDistance > distanceRecord }
             }.reduce { acc, i -> acc * i }
     }
+
+    @Test
+    fun part2Example() {
+        val input =
+            $"""
+            Time:      7  15   30
+            Distance:  9  40  200
+            """.trimIndent()
+                .lines()
+        val result = part2(input)
+        assertThat(result).isEqualTo(71503)
+    }
+
+    @Test
+    fun part2Input() {
+        val lines = fileInput("Day6.txt").readLines()
+        val duration = measureTime {
+            val result = part2(lines)
+            assertThat(result).isEqualTo(34655848)
+        }
+        println("part2 $duration")
+    }
+
+    fun part2(input: List<String>): Long {
+        val (raceTime, distanceRecord) = input.map { str ->
+            str.substringAfter(':')
+                .filter(Char::isDigit)
+                .toLong()
+        }.zipWithNext()
+            .first()
+
+        val lowestHoldTimeToBeatTheRecord = lowerBinarySearch(raceTime) { holdTime ->
+            val travelDistance = holdTime * (raceTime - holdTime)
+            travelDistance > distanceRecord
+        }
+        val highestHoldTimeToBeatTheRecord = lowerBinarySearch(raceTime) { holdTime ->
+            val travelDistance = holdTime * (raceTime - holdTime)
+            // flip the condition to find first highest value
+            travelDistance < distanceRecord
+        }
+        return highestHoldTimeToBeatTheRecord - lowestHoldTimeToBeatTheRecord
+    }
+
+    fun lowerBinarySearch(raceTime: Long, ok: (Long) -> Boolean ): Long {
+        var l = -1L
+        var r = raceTime
+        while (r - l > 1) {
+            val holdTime = (l + r) / 2
+            if (ok(holdTime)) {
+                r = holdTime
+            } else {
+                l = holdTime
+            }
+        }
+        return r
+    }
 }
