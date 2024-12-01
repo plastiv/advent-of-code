deea54399ea98bce138c5d86eaff7b08bcae5c7a
Sergii Pechenizkyi <plastivreg@gmail.com>
2024-12-01 02:13:34 +0100

    2023 day 9 part 2

--- src/test/kotlin/aoc2023/Day9.kt
+++ src/test/kotlin/aoc2023/Day9.kt
@@ -44,4 +44,43 @@ class Day9 {
             .map { (first, second) -> second - first }
         return records.last() + predictNextValue(newList)
     }
+
+    @Test
+    fun part2Example() {
+        val input =
+            $"""
+            0 3 6 9 12 15
+            1 3 6 10 15 21
+            10 13 16 21 30 45
+            """.trimIndent()
+                .lines()
+        val result = part2(input)
+        assertThat(result).isEqualTo(2)
+    }
+
+    @Test
+    fun part2Input() {
+        val lines = fileInput("Day9.txt").readLines()
+        val duration = measureTime {
+            val result = part2(lines)
+            assertThat(result).isEqualTo(1077)
+        }
+        println("part1 $duration")
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.map { str ->
+            str.split(' ').map { it.toInt() }
+        }.sumOf { ints ->
+            predictPrevValue(records = ints)
+        }
+    }
+
+    fun predictPrevValue(records: List<Int>): Int {
+        if (records.all { it == 0 }) return 0
+
+        val newList = records.windowed(2)
+            .map { (first, second) -> second - first }
+        return records.first() - predictPrevValue(newList)
+    }
 }
