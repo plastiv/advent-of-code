ec843a85c32aae86bcd1c49d52620f0ba3c8313f
Sergii Pechenizkyi <plastivreg@gmail.com>
2024-12-02 17:27:28 +0100

    2024 day 2 refactor `zipWithNext`, `x in 1..3` and `subList`

--- src/test/kotlin/aoc2024/Day2.kt
+++ src/test/kotlin/aoc2024/Day2.kt
@@ -2,7 +2,6 @@ package aoc2024
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import kotlin.math.abs
 import kotlin.test.Test
 import kotlin.time.measureTime
 
@@ -37,31 +36,14 @@ class Day2 {
         return input.map { str ->
             str.split(' ').filterNot(String::isEmpty).map(String::toInt)
         }
-            .filter { ints ->
-                ints
-                    .windowed(2)
-                    .all { (first, second): List<Int> ->
-                        when (abs(second - first)) {
-                            1, 2, 3 -> true
-                            else -> false
-                        }
-                    }
-            }.filter { ints ->
-                val allIncreasing = ints
-                    .windowed(2)
-                    .all { (first, second) ->
-                        first <= second
-                    }
-                val allDecreasing = ints
-                    .windowed(2)
-                    .all { (first, second) ->
-                        first >= second
-                    }
-                allIncreasing || allDecreasing
+            .count { ints ->
+                ints.areAscending() ||
+                        ints.reversed().areAscending()
             }
-            .also(::println)
-            .count()
     }
+
+    fun List<Int>.areAscending(): Boolean = this.zipWithNext()
+        .all { (first, second) -> (second - first) in 1..3 }
 
     @Test
     fun part2Example() {
@@ -86,92 +68,35 @@ class Day2 {
             val result = part2(lines)
             assertThat(result).isEqualTo(493)
         }
-        println("part1 $duration")
+        println("part2 $duration")
     }
-
-    fun List<Int>.allInc(): Boolean = this.windowed(2)
-        .all { (first, second) ->
-            when (second - first) {
-                1, 2, 3 -> true
-                else -> false
-            }
-        }
-
-    
 
     fun part2(input: List<String>): Int {
         return input.map { str ->
             str.split(' ').filterNot(String::isEmpty).map(String::toInt)
         }
-            .filter { ints: List<Int> ->
-                val check = chekc(ints)
-                println("$check ${ints.toString()}")
-                return@filter check
+            .count { ints: List<Int> ->
+                areAscending2(ints) || areAscending2(ints.reversed())
             }
-            .also(::println)
-            .count()
     }
 
-    private fun chekc(ints: List<Int>): Boolean {
-        if (ints.allInc()) {
-            return true
+    private fun areAscending2(ints: List<Int>): Boolean {
+
+        val find = ints.withIndex()
+            .zipWithNext()
+            .find { (first, second) ->
+                (second.value - first.value) !in 1..3
+            }
+
+        if (find == null) {
+            return true // all valid
         }
 
-        if (removeSecondNonInc(ints).allInc()) {
-            return true
-        }
-
-        if (removeFirstNonInc(ints).allInc()) {
-            return true
-        }
-
-        if (ints.reversed().allInc()) {
-            return true
-        }
-
-        if (removeSecondNonInc(ints.reversed()).allInc()) {
-            return true
-        }
-
-        if (removeFirstNonInc(ints.reversed()).allInc()) {
-            return true
-        }
-        return false
+        val (first, second) = find
+        return ints.removeIndex(first.index).areAscending() || ints.removeIndex(second.index).areAscending()
     }
 
-    private fun removeSecondNonInc(ints: List<Int>): MutableList<Int> {
-        val windowed = ints.withIndex()
-            .windowed(2)
-
-        val find = windowed.find { (first, second) ->
-            when (second.value - first.value) {
-                1, 2, 3 -> false
-                else -> true
-            }
-        }
-//        ints.toMutableList().removeAll { i ->  }
-
-        val (_, second) = find!!
-        val toMutableList = ints.toMutableList()
-        toMutableList.removeAt(second.index)
-        return toMutableList
-    }
-
-    private fun removeFirstNonInc(ints: List<Int>): MutableList<Int> {
-        val windowed = ints.withIndex()
-            .windowed(2)
-
-        val find = windowed.find { (first, second) ->
-            when (second.value - first.value) {
-                1, 2, 3 -> false
-                else -> true
-            }
-        }
-//        ints.toMutableList().removeAll { i ->  }
-
-        val (first, _) = find!!
-        val toMutableList = ints.toMutableList()
-        toMutableList.removeAt(first.index)
-        return toMutableList
+    fun List<Int>.removeIndex(index: Int): List<Int> {
+        return subList(0, index) + subList(index + 1, size)
     }
 }
