ce15c3f47c1254ace2933fce044cd17894252d99
Sergii Pechenizkyi <plastivreg@gmail.com>
2024-12-02 16:52:12 +0100

    2024 day 2 part 2

--- src/test/kotlin/aoc2024/Day2.kt
+++ src/test/kotlin/aoc2024/Day2.kt
@@ -62,4 +62,116 @@ class Day2 {
             .also(::println)
             .count()
     }
+
+    @Test
+    fun part2Example() {
+        val input =
+            $"""
+            7 6 4 2 1
+            1 2 7 8 9
+            9 7 6 2 1
+            1 3 2 4 5
+            8 6 4 4 1
+            1 3 6 7 9
+            """.trimIndent()
+                .lines()
+        val result = part2(input)
+        assertThat(result).isEqualTo(4)
+    }
+
+    @Test
+    fun part2Input() {
+        val lines = fileInput("Day2.txt").readLines()
+        val duration = measureTime {
+            val result = part2(lines)
+            assertThat(result).isEqualTo(493)
+        }
+        println("part1 $duration")
+    }
+
+    fun List<Int>.allInc(): Boolean = this.windowed(2)
+        .all { (first, second) ->
+            when (second - first) {
+                1, 2, 3 -> true
+                else -> false
+            }
+        }
+
+    
+
+    fun part2(input: List<String>): Int {
+        return input.map { str ->
+            str.split(' ').filterNot(String::isEmpty).map(String::toInt)
+        }
+            .filter { ints: List<Int> ->
+                val check = chekc(ints)
+                println("$check ${ints.toString()}")
+                return@filter check
+            }
+            .also(::println)
+            .count()
+    }
+
+    private fun chekc(ints: List<Int>): Boolean {
+        if (ints.allInc()) {
+            return true
+        }
+
+        if (removeSecondNonInc(ints).allInc()) {
+            return true
+        }
+
+        if (removeFirstNonInc(ints).allInc()) {
+            return true
+        }
+
+        if (ints.reversed().allInc()) {
+            return true
+        }
+
+        if (removeSecondNonInc(ints.reversed()).allInc()) {
+            return true
+        }
+
+        if (removeFirstNonInc(ints.reversed()).allInc()) {
+            return true
+        }
+        return false
+    }
+
+    private fun removeSecondNonInc(ints: List<Int>): MutableList<Int> {
+        val windowed = ints.withIndex()
+            .windowed(2)
+
+        val find = windowed.find { (first, second) ->
+            when (second.value - first.value) {
+                1, 2, 3 -> false
+                else -> true
+            }
+        }
+//        ints.toMutableList().removeAll { i ->  }
+
+        val (_, second) = find!!
+        val toMutableList = ints.toMutableList()
+        toMutableList.removeAt(second.index)
+        return toMutableList
+    }
+
+    private fun removeFirstNonInc(ints: List<Int>): MutableList<Int> {
+        val windowed = ints.withIndex()
+            .windowed(2)
+
+        val find = windowed.find { (first, second) ->
+            when (second.value - first.value) {
+                1, 2, 3 -> false
+                else -> true
+            }
+        }
+//        ints.toMutableList().removeAll { i ->  }
+
+        val (first, _) = find!!
+        val toMutableList = ints.toMutableList()
+        toMutableList.removeAt(first.index)
+        return toMutableList
+    }
 }
