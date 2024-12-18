98f6d49cf8bc0555640923350c0ef10bb796e343
Sergii Pechenizkyi <plastivreg@gmail.com>
2024-12-18 12:24:37 +0100

    2024 day 18 part 2

--- src/test/kotlin/aoc2024/Day18.kt
+++ src/test/kotlin/aoc2024/Day18.kt
@@ -38,8 +38,8 @@ class Day18 {
 2,0
             """.trimIndent()
                 .lines()
-        val result = part1(input)
-        assertThat(result).isEqualTo(0)
+        val result = part1(input, 12, 6, 6)
+        assertThat(result).isEqualTo(22)
     }
 
     @Test
@@ -47,8 +47,8 @@ class Day18 {
         val lines = fileInput("Day18.txt").readLines()
         val duration =
             measureTime {
-                val result = part1(lines)
-                assertThat(result).isEqualTo(0)
+                val result = part1(lines, 1024, 70, 70)
+                assertThat(result).isEqualTo(404)
             }
         println("part1 $duration")
     }
@@ -60,20 +60,81 @@ class Day18 {
 
     fun Bounds.inside(position: Positionm): Boolean = position.row in vertical && position.col in horizontal
 
-    fun part1(input: List<String>): Int {
+    fun part1(
+        input: List<String>,
+        iter: Int,
+        width: Int,
+        height: Int,
+    ): Int {
         val faultSegments =
             input
-                .take(1024)
                 .map { string ->
                     string.split(',').map(String::toInt)
                 }.map { (first, second) -> Positionm(second, first) }
 
+        val path = findPath(faultSegments, iter, width, height)
+        return path.size
+    }
+
+    @Test
+    fun part2Example1() {
+        val input =
+            """
+5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0
+            """.trimIndent()
+                .lines()
+        val result = part2(input, 12, 6, 6)
+        assertThat(result).isEqualTo(Positionm(1, 6))
+    }
+
+    @Test
+    fun part2Input() {
+        val lines = fileInput("Day18.txt").readLines()
+        val duration =
+            measureTime {
+                val result = part2(lines, 1024, 70, 70)
+                assertThat(result).isEqualTo(Positionm(60, 27))
+            }
+        println("part2 $duration")
+    }
+
+    fun findPath(
+        allFaults: List<Positionm>,
+        takeN: Int,
+        width: Int = 70,
+        height: Int = 70,
+    ): List<Positionm> {
+        val faultSegments = allFaults.take(takeN)
         var visited = mutableSetOf<Positionm>()
         val queue: Queue<Positionm> = LinkedList<Positionm>()
         val parentMap = mutableMapOf<Positionm, Positionm>()
         val start = Positionm(0, 0)
-        val end = Positionm(70, 70)
-        val bounds = Bounds(IntRange(0, 70), IntRange(0, 70))
+        val end = Positionm(height, width)
+        val bounds = Bounds(IntRange(0, height), IntRange(0, width))
         queue.add(start)
         while (queue.isNotEmpty()) {
             val candidate = queue.poll()
@@ -103,6 +164,13 @@ class Day18 {
                 }
             }
         }
+
+        println("")
+        println("iter $takeN ${allFaults[takeN]}")
+        println("")
+        if (end !in parentMap) {
+            return emptyList()
+        }
         val shortPath =
             buildList {
                 var current = end
@@ -111,46 +179,45 @@ class Day18 {
                     current = parentMap[current]!!
                 }
             }
-        bounds.horizontal
-            .map { i ->
-                bounds.vertical
-                    .map { j ->
-                        val pos = Positionm(i, j)
-                        if (pos in faultSegments) {
-                            '#'
-//                        } else if (pos in visited) {
-                        } else if (pos in shortPath) {
-                            '0'
-                        } else {
-                            '.'
-                        }
-                    }.joinToString("")
-            }.joinToString("\n")
-            .also(::println)
-        return shortPath.size
+//        bounds.horizontal.joinToString("\n") { i ->
+//            bounds.vertical
+//                .map { j ->
+//                    val pos = Positionm(i, j)
+//                    if (pos in faultSegments) {
+//                        '#'
+//                        //                        } else if (pos in visited) {
+//                    } else if (pos in shortPath) {
+//                        '0'
+//                    } else {
+//                        '.'
+//                    }
+//                }.joinToString("")
+//        }.also(::println)
+
+        return shortPath
     }
 
-    @Test
-    fun part2Example1() {
-        val input =
-            """
+    fun part2(
+        input: List<String>,
+        startIter: Int,
+        width: Int,
+        height: Int,
+    ): Positionm {
+        val faultSegments =
+            input
+                .map { string ->
+                    string.split(',').map(String::toInt)
+                }.map { (first, second) -> Positionm(second, first) }
 
-            """.trimIndent()
-                .lines()
-        val result = part2(input)
-        assertThat(result).isEqualTo(117440)
-    }
-
-    @Test
-    fun part2Input() {
-        val lines = fileInput("Day18.txt").readLines()
-        val duration =
-            measureTime {
-                val result = part2(lines)
-                assertThat(result).isEqualTo(117440)
+        val end = Positionm(height, width)
+        var iter = faultSegments.size - 1
+        while (true) {
+            val path = findPath(faultSegments, iter, width, height)
+            if (end in path) {
+                break
             }
-        println("part2 $duration")
+            iter--
+        }
+        return faultSegments[iter]
     }
-
-    fun part2(input: List<String>): Long = 0
 }
