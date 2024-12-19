52ae4ce10398776d41bd6a1e692e48c0d72dcc28
Sergii Pechenizkyi <plastivreg@gmail.com>
2024-12-19 15:50:09 +0100

    2024 day 19 part 1

---
+++ src/test/kotlin/aoc2024/Day19.kt
@@ -0,0 +1,92 @@
+package aoc2024
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Test
+import kotlin.time.measureTime
+
+class Day19 {
+    @Test
+    fun part1Example1() {
+        val input =
+            """
+            r, wr, b, g, bwu, rb, gb, br
+
+            brwrr
+            bggr
+            gbbr
+            rrbgbr
+            ubwu
+            bwurrg
+            brgr
+            bbrgwb
+            """.trimIndent()
+                .lines()
+        val result = part1(input)
+        assertThat(result).isEqualTo(6)
+    }
+
+    @Test
+    fun part1Input() {
+        val lines = fileInput("Day19.txt").readLines()
+        val duration =
+            measureTime {
+                val result = part1(lines)
+                assertThat(result).isEqualTo(260)
+                // 243 is too low
+            }
+        println("part1 $duration")
+    }
+
+    fun part1(input: List<String>): Int {
+        val alphabet =
+            input
+                .first()
+                .split(", ")
+                .map(String::reversed)
+                .sortedByDescending { string -> string.length }
+        val words =
+            input
+                .drop(2)
+                .map(String::reversed)
+
+        fun f(tail: String): Boolean {
+            return if (tail.isEmpty()) {
+                true
+            } else {
+                alphabet
+                    .any { prefix ->
+                        return@any if (tail.startsWith(prefix)) {
+                            f(tail.removePrefix(prefix))
+                        } else {
+                            false
+                        }
+                    }
+            }
+        }
+        return words.count { f(it) }
+    }
+
+    @Test
+    fun part2Example1() {
+        val input =
+            """
+            """.trimIndent()
+                .lines()
+        val result = part2(input)
+        assertThat(result).isEqualTo(0)
+    }
+
+    @Test
+    fun part2Input() {
+        val lines = fileInput("Day19.txt").readLines()
+        val duration =
+            measureTime {
+                val result = part2(lines)
+                assertThat(result).isEqualTo(0)
+            }
+        println("part2 $duration")
+    }
+
+    fun part2(input: List<String>) = 0
+}
